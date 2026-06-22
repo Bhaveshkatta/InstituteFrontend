c6aa2415b3a527410ffcb281fbaa8aba572758f7
Bhaveshkatta <bhaveshkatta942@gmail.com>
2026-06-22 20:01:50 +0530

    Initial Login Screen

---
+++ src/app/features/public/auth/login/login.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MATERIAL_IMPORTS } from '../../../../shared/ui/material.imports';
+
+@Component({
+  selector: 'app-login',
+  imports: [ ...MATERIAL_IMPORTS ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  hidePassword = signal(true);
+  isLoading = signal(false);
+  loginSuccess = signal(false);
+
+  togglePassword() {
+    this.hidePassword.update((val) => !val);
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading.set(true);
+
+    // Simulate an API call
+    setTimeout(() => {
+      this.isLoading.set(false);
+      this.loginSuccess.set(true);
+      
+      // Auto-hide success state after 3 seconds
+      setTimeout(() => {
+        this.loginSuccess.set(false);
+      }, 3000);
+    }, 1500);
+  }
+}
---
+++ src/app/shared/ui/material.imports.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+
+import {
+FormsModule,
+ReactiveFormsModule
+} from '@angular/forms';
+
+import {
+MatInputModule
+} from '@angular/material/input';
+
+import {
+MatButtonModule
+} from '@angular/material/button';
+
+import {
+MatCheckboxModule
+} from '@angular/material/checkbox';
+
+import {
+MatIconModule
+} from '@angular/material/icon';
+
+import {
+MatFormFieldModule
+} from '@angular/material/form-field';
+
+export const MATERIAL_IMPORTS=[
+
+CommonModule,
+
+FormsModule,
+
+ReactiveFormsModule,
+
+MatInputModule,
+
+MatButtonModule,
+
+MatCheckboxModule,
+
+MatIconModule,
+
+MatFormFieldModule,
+
+];
